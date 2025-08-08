61dbc76ef6a67b388ad3a1a70cd9ef46a268b962
tim-rayner <tim.rayner2020@gmail.com>
2025-08-08 18:12:04 +0100

    init: initialised api

---
+++ apps/engine-tone/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'engine-tone',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/engine-tone',
+};
